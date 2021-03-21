08281f82ca9c186aff8ce728867845d933f23523
DenisKoltsov1 <dublin87@mail.ru>
2021-03-21 17:53:20 +0300

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,67 +1,18 @@
 
     import React ,{Component} from 'react';
     import './App.css';
-    
+    import Header from './components/Header.js';
+    import Nav from './components/Nav';
+    import Profile from './components/Profile';
     
     const App = () => {
       return (
         <div className="app-wraper">
-        <header className="header">< img  src="https://s.starladder.com/uploads/team_logo/d/3/4/6/meta_tag_37934c7776a434dca3e5fa5d2c166e0c.jpg"/></header>
-    
-        <nav className="nav" >
-
-            <div>
-              <a>
-                Profile
-              </a>
-            </div>
-            <div>
-              <a>
-                Messages
-              </a>
-            </div>
-            <div>
-              <a>
-                News
-              </a>
-            </div>
-            <div>
-              <a>
-                Music
-              </a>
-            </div>
-            <div>
-              <a>
-                Settings
-              </a>
-            </div>
-        </nav>
-    
-      <div className="content">
-        <div>
-            <img src="https://avatars.mds.yandex.net/get-zen_doc/1781567/pub_5cbd802a3456ac00b393fda8_5cbd807170f6f500b0ddb979/scale_1200"/>
-       </div>
-
-
-        <div>
-        <img src="http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1Rk3xj3SNsykl6tc1aFKIVZ6aaKTM5SRkZCeTgDn6uOyic"/>
-       </div>
-
-       <div>
-         MyPOst
-        </div>
-       <div>
-        NewPOst
-        </div>
-       <div>
-         post1
-        </div>
-       <div>
-         post2
-      </div>
-       
-    
-      </div>
+       <Header/>
+       <Header/>
+       <Nav/>
+        <Profile/>
+      
       </div>
       );
     }
