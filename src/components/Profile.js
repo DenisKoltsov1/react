0406fba787a9f29d252932baad085153bfc4af5c
DenisKoltsov1 <dublin87@mail.ru>
2021-03-21 17:53:20 +0300

    Add Components

---
+++ src/components/Profile.js
@@ -0,0 +1,38 @@
+import React ,{Component} from 'react';
+
+
+const  Profile = ()=> 
+{
+    return    <div className="content">
+    <div>
+        <img src="https://avatars.mds.yandex.net/get-zen_doc/1781567/pub_5cbd802a3456ac00b393fda8_5cbd807170f6f500b0ddb979/scale_1200"/>
+   </div>
+
+
+    <div>
+    <img src="http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1Rk3xj3SNsykl6tc1aFKIVZ6aaKTM5SRkZCeTgDn6uOyic"/>
+   </div>
+
+   <div>
+     MyPOst
+    </div>
+   <div>
+    NewPOst
+    </div>
+   <div>
+     post1
+    </div>
+   <div>
+     post2
+  </div>
+   
+
+  </div>
+
+    
+
+
+}
+
+
+export  default Profile
